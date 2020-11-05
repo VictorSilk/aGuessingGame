1030bb36bbecf8b5b7ea11b913f9983d1a80bd59
Victor <victor.silkisberg@outlook.com>
2020-11-05 12:48:02 +0100

    added chmod permission

--- run_game.sh
+++ run_game.sh
@@ -4,25 +4,23 @@ echo "Victor Silkisberg"
 
 mkdir Victor_Silkisberg_labb
 
-cp Guesser.java Guesser2.java
-
-mv Guesser2.java ./Victor_Silkisberg_labb
+cp -R "*.java" "/Victor_Silkisberg_labb/"
 
 cd ./Victor_Silkisberg_labb
 
 echo -n "Running game from "
 pwd
 
-Javac Guesser2.java
+Javac Guesser.java
 
 echo "Compiling game..."
 
 echo "Starting game"
 
-java Guesser2
+java Guesser
 
 echo "Done"
 
-rm -r *.class
+rm  *.class
 
 ls ./Victor_Silkisberg_labb
